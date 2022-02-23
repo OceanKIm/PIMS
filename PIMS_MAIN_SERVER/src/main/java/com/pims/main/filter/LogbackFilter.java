11ced67a0bf3443acd8ab83bf332739862ffd91c
HaeseoungKim <gotmd37@naver.com>
2022-02-24 00:43:55 +0900

    [feat] logging setting add
    log4jdbc, logback 기능 추가

---
+++ PIMS_MAIN_SERVER/src/main/java/com/pims/main/filter/LogbackFilter.java
@@ -0,0 +1,29 @@
+package com.pims.main.filter;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+
+/**
+ * LogbackFilter
+ * : 로그에서 해당 문자열을 포함하면 제외시킨다.
+ *
+ * @author hskim
+ * @version 1.0.0
+ * 작성일 2022-02-24
+**/
+public class LogbackFilter extends Filter<ILoggingEvent> {
+
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+        if (event.getMessage().contains("NOT_SQL_LOG")) {
+            return FilterReply.DENY;
+        }else{
+            return FilterReply.ACCEPT;
+        }
+    }
+
+}
+
+
