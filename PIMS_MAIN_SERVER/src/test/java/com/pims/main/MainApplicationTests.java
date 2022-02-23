e2f4cc77d81bca837478340bf80a39f004d5990f
HaeseoungKim <gotmd37@naver.com>
2022-02-23 19:29:33 +0900

    first commit
    프로젝트 생성

---
+++ PIMS_MAIN_SERVER/src/test/java/com/pims/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pims.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
