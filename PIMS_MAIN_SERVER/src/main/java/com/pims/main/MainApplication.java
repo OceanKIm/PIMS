b38acc709e1c297f44cec55cd802aa91b2475c21
HaeseoungKim <gotmd37@naver.com>
2022-02-23 21:27:59 +0900

    [feat] setting database
    데이터 베이스 커넥션 설정 완료

--- PIMS_MAIN_SERVER/src/main/java/com/pims/main/MainApplication.java
+++ PIMS_MAIN_SERVER/src/main/java/com/pims/main/MainApplication.java
@@ -3,6 +3,15 @@ package com.pims.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * MainApplication
+ * : 메인 서버 스타트 클래스
+ * 
+ * @author hskim
+ * @version 1.0.0
+ * 작성일 2022-02-23
+**/
 @SpringBootApplication
 public class MainApplication {
 
