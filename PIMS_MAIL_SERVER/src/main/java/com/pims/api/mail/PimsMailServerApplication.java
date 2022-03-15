2609d4b783e6a0d0738181fccfefb16e840d1898
HaeseoungKim <gotmd37@naver.com>
2022-03-15 17:30:02 +0900

    [initialization] start pims mail server

---
+++ PIMS_MAIL_SERVER/src/main/java/com/pims/api/mail/PimsMailServerApplication.java
@@ -0,0 +1,13 @@
+package com.pims.api.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimsMailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimsMailServerApplication.class, args);
+	}
+
+}
---
+++ PIMS_MAIL_SERVER/src/test/java/com/pims/api/mail/PimsMailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pims.api.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimsMailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
